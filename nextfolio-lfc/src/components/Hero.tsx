80ac4d3e8dbcd59b68ed6a159faa404e67e674e1
Leonardo Fuentes Claros <leonardofuentesclaros@gmail.com>
2025-01-04 21:20:42 -0400

    Add hero section

---
+++ nextfolio-lfc/src/components/Hero.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import Link from 'next/link'
+import MagicBtn from './ui/MagicBtn'
+import { FaLocationArrow } from 'react-icons/fa'
+
+function Hero() {
+  return (
+    <div className='pb-20'> {/* pt-36 */}
+      <div>
+        <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
+        <Spotlight className='top-10  left-full h-[80vh] w-[50vw]' fill='purple' />
+        <Spotlight className='top-28  left-80 h-[80vh] w-[50vw]' fill='#A0E7FF' />
+      </div>
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex justify-center items-center top-0 left-0">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        <div className='flex justify-center relative my-20 z-10'>
+          <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+              Crafting Digital Experiences
+            </h2>
+            <TextGenerateEffect
+              className='text-center text-[40px] md:text-5xl lg:text-6xl'
+              words='Welcome to my portfolio'
+            />
+            <p className='text-center md:tracking-wider mb-4 text-sm md:tex-lg lg:text-2xl'>
+              Hi, I&apos;m Leonardo, a Software Developer based in Bolivia.
+            </p>
+            <Link href={'#about'}>
+              <MagicBtn
+                title='Explore my work'
+                icon={<FaLocationArrow />}
+                position='right'
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Hero
---
+++ nextfolio-lfc/src/components/ui/TextGenerateEffect.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { useEffect } from "react";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+export const TextGenerateEffect = ({
+  words,
+  className,
+  filter = true,
+  duration = 0.5,
+}: {
+  words: string;
+  className?: string;
+  filter?: boolean;
+  duration?: number;
+}) => {
+  const [scope, animate] = useAnimate();
+  let wordsArray = words.split(" ");
+  useEffect(() => {
+    animate(
+      "span",
+      {
+        opacity: 1,
+        filter: filter ? "blur(0px)" : "none",
+      },
+      {
+        duration: duration ? duration : 1,
+        delay: stagger(0.2),
+      }
+    );
+  }, [scope.current]);
+
+  const renderWords = () => {
+    return (
+      <motion.div ref={scope}>
+        {wordsArray.map((word, idx) => {
+          const dynamicClasses = idx > 2 ? 'text-[#00C6FF]' : 'dark:text-white text-black';
+          const blurEffect = filter ? "blur(10px)" : "none";
+          return (
+            <motion.span
+              key={word + idx}
+              className={`${dynamicClasses} opacity-0`}
+              style={{
+                filter: blurEffect
+              }}
+            >
+              {word}{" "}
+            </motion.span>
+          );
+        })}
+      </motion.div>
+    );
+  };
+
+  return (
+    <div className={cn("font-bold", className)}>
+      <div className="my-4">
+        <div className=" dark:text-white text-black leading-snug tracking-wide">
+          {renderWords()}
+        </div>
+      </div>
+    </div>
+  );
+};
